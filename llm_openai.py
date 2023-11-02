658c3e66f74f72ef1a08a52d9302d9c525be3b05
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2023-11-02 15:46:21 +0800

    Factorize llm_openai.py

--- llm_openai.py
+++ llm_openai.py
@@ -1,42 +1,27 @@
 from omegaconf import DictConfig
 from langchain.chat_models import ChatOpenAI
 from langchain.chains.question_answering import load_qa_chain
-from langchain.vectorstores.chroma import Chroma
-
 
 class OpenAIHandler:
-    def __init__(self, cfg: DictConfig, query: str, db: Chroma, 
-                 chain_type="stuff", verbose="true"):
-        self.api_key = cfg.openAI.api_key
-        self.model = cfg.openAI.model
-        self.temperature = cfg.openAI.temperature
-        self.max_tokens = cfg.openAI.max_tokens
-        self.query = query
-        self.db = db
-        self.chain_type = chain_type
-        self.verbose = verbose
 
-    def load_model(self):
+    def load_model(self, cfg: DictConfig):
         '''
         Loads the OpenAI model.
+
+        Returns:
+            The OpenAI model.
         '''
-        return ChatOpenAI(openai_api_key=self.api_key, model_name=self.model, 
-                          temperature=self.temperature, max_tokens=self.max_tokens)
+        openAI = cfg.openAI
+        return ChatOpenAI(openai_api_key=openAI.api_key, model_name=openAI.model, 
+                          temperature=openAI.temperature, max_tokens=openAI.max_tokens)
     
 
-    def setup_chain(self):
+    def setup_chain(self, cfg: DictConfig, chain_type="stuff", verbose="false"):
         '''
         Sets up the Q & A chain.
-        '''
-        llm = self.load_model()
-        return load_qa_chain(llm=llm, chain_type=self.chain_type, verbose=self.verbose)
-    
 
-    def get_response(self):
+        Returns:
+            The Q & A chain.
         '''
-        Method to generate response from the query.
-        '''
-        matching_docs = self.db.similarity_search(self.query)
-        chain = self.setup_chain()
-        answer = chain.run(input_documents=matching_docs, question=self.query)
-        return answer
+        llm = self.load_model(cfg)
+        return load_qa_chain(llm=llm, chain_type=chain_type, verbose=verbose)
