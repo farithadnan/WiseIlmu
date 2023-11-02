8476f47fd6980d4157641bba548cb19467b9efec
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2023-11-02 15:46:38 +0800

    Factorize loader.py

--- loader.py
+++ loader.py
@@ -1,38 +1,33 @@
+import chromadb
 from langchain.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from langchain.vectorstores.chroma import Chroma
 
 class DocHandler:
-
-    def __init__(self, directory_path: str):
-        '''
-        Initializes the DocHandler with a directory path
-
-        Args:
-            directory_path (str): The path to the directory containing the documents
-        '''
-        self.directory_path = directory_path
-
-
-    def get_vector_db(self):
+    def get_vector_db(self, embedding_model: str, document_dir: str, 
+                      chunk_size=1000, chunk_overlap=20):
         '''
         Fetch vector representation of the documents in the directory.
+
+        Args:
+            embedding_model (str): The name of the embedding model.
+            document_dir (str): The directory containing the documents.
+            chunk_size (int): The size of the chunks.
+            chunk_overlap (int): The overlap between the chunks.
 
         Returns:
             Vector representation of the documents.
         '''
-
-        # Embed chunks into vectors
-        embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
-        documents = self.split_docs()
+        documents = self.process_docs(document_dir, chunk_size, chunk_overlap)
+        embeddings = SentenceTransformerEmbeddings(model_name=embedding_model)
         db = Chroma.from_documents(documents, embeddings)
         return db
 
 
-    def split_docs(self, chunk_size=1000, chunk_overlap=20):
+    def process_docs(self, document_dir: str, chunk_size=1000, chunk_overlap=20):
         '''
-        Splits the documents into smaller chunks.
+        Load & split the documents into smaller chunks.
 
         Args:
             chunk_size (int): The size of the chunks.
@@ -41,19 +36,12 @@ class DocHandler:
         Returns:
             List of documents.
         '''
+        # Load documents
+        loader = DirectoryLoader(document_dir)
+        documents = loader.load()
+        # Split documents into chunks
         text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-        documents = self.load_docs()
         docs = text_splitter.split_documents(documents=documents)
+
         return docs
     
-
-    def load_docs(self):
-        '''
-        Loads the documents from the directory.
-
-        Returns:
-            The loaded documents.
-        '''
-        loader = DirectoryLoader(self.directory_path)
-        documents = loader.load()
-        return documents
