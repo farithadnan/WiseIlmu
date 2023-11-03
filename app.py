1743eb35423e50b114d1c658989135dfb896ca44
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2023-11-03 12:52:02 +0800

    Refactor chatbot setup and launch

--- app.py
+++ app.py
@@ -1,22 +1,18 @@
-import os
 import hydra
 import logging
 from omegaconf import DictConfig
 from chatbot import ChatBot
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 @hydra.main(version_base=None, config_path="config", config_name="config")
 def main(cfg: DictConfig):
     logger = logging.getLogger(__name__)
 
     try:
-        # Get directory path of the documents
-        current_dir  = os.path.dirname(os.path.abspath(__file__))
-        document_dir = os.path.join(current_dir, "documents")
-
-        # Setup & launch the chatbot
-        chatbotHandler = ChatBot()
-        chatbot = chatbotHandler.setup_chatbot(cfg, document_dir, cfg.embeddings.model)
-        chatbotHandler.launch_chatbot(chatbot=chatbot)
+        # launch the chatbot
+        chatbot = ChatBot(cfg)
+        chatbot.launch()
     
     except Exception as e:
         logger.exception(e)
--- chatbot.py
+++ chatbot.py
@@ -4,51 +4,49 @@ from omegaconf import DictConfig
 from loader import DocHandler
 
 class ChatBot: 
-    def launch_chatbot(self, chatbot):
-        try:
-            iface = gr.Interface(
-                fn=chatbot,
-                inputs=gr.components.Textbox(lines=7, label="Question From User", placeholder="Enter your question here"),
-                outputs=gr.components.Textbox(lines=7, label="Response from Chatbot", placeholder="Chatbot's response will appear here"),
-                title="🤖Chatbot (OpenAI)"
-            )
-            iface.launch()
-        except Exception as e:
-            print(f"Error while launching the chatbot: {e}")
+    def __init__(self, cfg: DictConfig):
+        self.cfg = cfg
+        self.documents_dir = cfg.documents_dir
+        self.embedding_model = cfg.embeddings.model
 
-    def setup_chatbot(self, cfg: DictConfig, document_dir: str, embedding_model: str):
+    def setup(self):
         '''
         Sets up the chatbot.
-
-        Args:
-            cfg (DictConfig): The configuration.
-            document_dir (str): The directory containing the documents.
-            embedding_model (str): The name of the embedding model.
-
+        
         Returns:
             The chatbot.
         '''
         try:
             # Get vector database
-            docHandler = DocHandler()
-            db = docHandler.get_vector_db(embedding_model, document_dir)
+            docHandler = DocHandler(self.cfg)
+            db = docHandler.get_vector_db()
 
             def chatbot(input_text):
-                try:
-                    # Initialize Q & A chain for OpenAI
-                    openAI = OpenAIHandler()
-                    qa = openAI.setup_chain(cfg)
+                # Initialize Q & A chain for OpenAI
+                openAI = OpenAIHandler(self.cfg)
+                qa = openAI.setup_chain()
 
-                    # Get matching documents based on input text
-                    matching_docs = db.similarity_search(input_text)
-                    answer = qa.run(input_documents=matching_docs, question=input_text)
-                    return answer
-                except Exception as e:
-                    raise RuntimeError(f"Error processing chatbot request: {e}")
+                # Get matching documents based on input text
+                matching_docs = db.similarity_search(input_text)
+                answer = qa.run(input_documents=matching_docs, question=input_text)
+                return answer
 
             return chatbot
         except Exception as e:
             raise RuntimeError(f"Error while setting up the chatbot: {e}")
+
+    def launch(self):
+        '''
+        Launches the chatbot.
+        '''
+        iface = gr.Interface(
+            fn=self.setup(),
+            inputs=gr.components.Textbox(lines=7, label="Question From User", placeholder="Enter your question here"),
+            outputs=gr.components.Textbox(lines=7, label="Response from Chatbot", placeholder="Chatbot's response will appear here"),
+            title="🤖Chatbot (OpenAI)"
+        )
+        iface.launch()
+
 
 
 
