06c2ab7461459be25715599437ea1e2466a2cca1
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2023-11-03 12:52:36 +0800

    Refactor DocHandler class to use configuration object

--- loader.py
+++ loader.py
@@ -5,32 +5,29 @@ from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddi
 from langchain.vectorstores.chroma import Chroma
 
 class DocHandler:
-    def get_vector_db(self, embedding_model: str, document_dir: str, 
-                      chunk_size=1000, chunk_overlap=20):
+    def __init__ (self, cfg):
+        self.documents_dir = cfg.documents_dir
+        self.embedding_model = cfg.embeddings.model
+
+    def get_vector_db(self, chunk_size=1000, chunk_overlap=20):
         '''
         Fetch vector representation of the documents in the directory.
 
         Args:
-            embedding_model (str): The name of the embedding model.
-            document_dir (str): The directory containing the documents.
-            chunk_size (int): The size of the chunks.
+            chunk_size (int): The size of the chunks. 
             chunk_overlap (int): The overlap between the chunks.
 
         Returns:
             Vector representation of the documents.
         '''
-        try:
-            documents = self.process_docs(document_dir, chunk_size, chunk_overlap)
-            embeddings = SentenceTransformerEmbeddings(model_name=embedding_model)
-            db = Chroma.from_documents(documents, embeddings)
-            return db
-        except FileNotFoundError as e:
-            raise FileNotFoundError(f"Directory {document_dir} not found: {e}")
-        except Exception as e:
-            raise RuntimeError(f"Error while fetching vector representation of the documents: {e}")
+        documents = self.process_docs(chunk_size, chunk_overlap)
+        embeddings = SentenceTransformerEmbeddings(model_name=self.embedding_model)
+        db = Chroma.from_documents(documents, embeddings)
+        return db
 
 
-    def process_docs(self, document_dir: str, chunk_size=1000, chunk_overlap=20):
+
+    def process_docs(self, chunk_size=1000, chunk_overlap=20):
         '''
         Load & split the documents into smaller chunks.
 
@@ -43,7 +40,7 @@ class DocHandler:
         '''
         try:
             # Load documents
-            loader = DirectoryLoader(document_dir)
+            loader = DirectoryLoader(self.documents_dir)
             documents = loader.load()
             # Split documents into chunks
             text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
@@ -51,7 +48,7 @@ class DocHandler:
 
             return docs
         except FileNotFoundError as e:
-            raise FileNotFoundError(f"Directory {document_dir} not found: {e}")
+            raise FileNotFoundError(f"Directory {self.documents_dir} not found: {e}")
         except Exception as e:
             raise RuntimeError(f"Error while loading & splitting the documents: {e}")
 
