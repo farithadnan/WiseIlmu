6acaa70d2baf1814ff5c2da0296b1016295997c9
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2023-11-03 12:52:29 +0800

    Refactored OpenAIHandler class to use instance variable for configuration

--- llm_openai.py
+++ llm_openai.py
@@ -1,11 +1,13 @@
-from cycler import K
 from omegaconf import DictConfig
 from langchain.chat_models import ChatOpenAI
 from langchain.chains.question_answering import load_qa_chain
 
 class OpenAIHandler:
 
-    def load_model(self, cfg: DictConfig):
+    def __init__ (self, cfg: DictConfig):
+        self.cfg = cfg
+
+    def load_model(self):
         '''
         Loads the OpenAI model.
 
@@ -13,22 +15,24 @@ class OpenAIHandler:
             The OpenAI model.
         '''
         try:
-            openAI = cfg.openAI
+            openAI = self.cfg.openAI
             return ChatOpenAI(openai_api_key=openAI.api_key, model_name=openAI.model, 
                                 temperature=openAI.temperature, max_tokens=openAI.max_tokens)
         except (AttributeError, KeyError) as e:
             raise ValueError(f"Invalid or missing configuration: {e}")
     
 
-    def setup_chain(self, cfg: DictConfig, chain_type="stuff", verbose="false"):
+    def setup_chain(self, chain_type="stuff", verbose="false"):
         '''
         Sets up the Q & A chain.
+
+        Args:
+            chain_type (str): The type of the chain.
+            verbose (str): Whether to print the logs or not.
 
         Returns:
             The Q & A chain.
         '''
-        try:
-            llm = self.load_model(cfg)
-            return load_qa_chain(llm=llm, chain_type=chain_type, verbose=verbose)
-        except Exception as e:
-            raise RuntimeError(f"Error while setting up the Q & A chain: {e}")
+        llm = self.load_model(self.cfg)
+        return load_qa_chain(llm=llm, chain_type=chain_type, verbose=verbose)
+
