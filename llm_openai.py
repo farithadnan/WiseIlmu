4fa18522227b3204a440d2e9e3d54ec275a5d6d4
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2023-11-11 13:00:01 +0800

    Refactor OpenAIHandler class to improve code structure and readability.

--- llm_openai.py
+++ llm_openai.py
@@ -1,6 +1,3 @@
-from pyexpat import model
-import re
-import openai
 from omegaconf import DictConfig
 from langchain.chat_models import ChatOpenAI
 from langchain.chains.question_answering import load_qa_chain
@@ -8,11 +5,11 @@ from langchain.chains.question_answering import load_qa_chain
 class OpenAIHandler:
 
     def __init__ (self, cfg: DictConfig, temperature, max_tokens):
-        self.cfg = cfg
+        self.config = cfg.openAI
         self.temperature = temperature
         self.max_tokens = max_tokens
 
-    def setup_chain(self, messages: any, chain_type="stuff", verbose="false"):
+    def setup_chain(self, chain_type="stuff", verbose="false"):
         '''
         Sets up the Q & A chain.
 
@@ -23,30 +20,15 @@ class OpenAIHandler:
         Returns:
             The Q & A chain.
         '''
-        llm = self.load_model(messages)
-        return load_qa_chain(llm=llm, chain_type=chain_type, verbose=verbose)
-
-    def load_model(self, messages: any):
-        '''
-        Loads the OpenAI model.
-
-        Returns:
-            The OpenAI model.
-        '''
         try:
-            config = self.cfg.openAI
-            response = openai.ChatCompletion.create(
-                model=config.model,
-                messages=messages,
-                temperature=self.temperature,
-                max_tokens=self.max_tokens,
-            )
             
-
-            return response
-
-            return ChatOpenAI(openai_api_key=config.api_key, model_name=config.model, 
-                                temperature=self.temperature, max_tokens=self.max_tokens)
+            # Setup OpenAI chat
+            llm = ChatOpenAI(openai_api_key=self.config.api_key, model_name=self.config.model, 
+                                temperature=self.temperature, max_tokens=self.max_tokens) 
+            
+            # Setup Q & A Chain
+            loaded_qa_chain = load_qa_chain(llm=llm, chain_type=chain_type, verbose=verbose)
+            return loaded_qa_chain
+    
         except (AttributeError, KeyError) as e:
             raise ValueError(f"Invalid or missing configuration: {e}")
-    
