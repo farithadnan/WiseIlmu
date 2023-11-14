c22f0a57029b058c60b777c20b29993860dfdcda
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2023-11-14 16:59:39 +0800

    Add data directory and update LLMOpenAI class.

--- app.py
+++ app.py
@@ -1,21 +1,118 @@
+import sys
 import hydra
-import logging
-from omegaconf import DictConfig
-from chatbot import ChatBot
+import pprint
+from halo import Halo
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+from omegaconf import DictConfig
+
+
+from langchain.vectorstores.chroma import Chroma
+
+
+from langchain.chains.combine_documents.base import BaseCombineDocumentsChain
+
+from llm_openai import LLMOpenAI
+from loader import Loader
+
+
+def generate_response(db: Chroma, qa: BaseCombineDocumentsChain, messages):
+    # Create a loading spinner
+    spinner = Halo(text='Loading...\n', spinner='dots')
+    spinner.start()
+
+    # Fetch latest user Input
+    latest_input = next((message for message in reversed(messages) if message.get("role") == "user"), None)
+
+    # Get matching documents based on input text
+    matching_docs = db.similarity_search(latest_input["content"])
+    answer = qa.run(input_documents=matching_docs, question=messages)
+
+    # Stop the spinner once the response is received
+    spinner.stop()
+
+    # Pretty-print the user message sent to the AI
+    pp = pprint.PrettyPrinter(indent=4)
+    print("Request:")
+    pp.pprint(messages)
+
+    return answer
+
 
 @hydra.main(version_base=None, config_path="config", config_name="config")
 def main(cfg: DictConfig):
-    logger = logging.getLogger(__name__)
+    # Load documents
+    documents_handler = Loader()
+    documents = documents_handler.load_documents(cfg.documents_dir)
 
-    try:
-        # launch the chatbot
-        chatbot = ChatBot(cfg)
-        chatbot.launch()
-    
-    except Exception as e:
-        logger.exception(e)
+    # Split documents into chunks
+    documents = documents_handler.split_documents(documents)
+
+    # Create Chroma DB
+    vector_db = documents_handler.create_vector_db(documents, cfg)
+
+    # Create Chain
+    openai_handler = LLMOpenAI()
+    qa_chain = openai_handler.get_qa_chain(cfg)
+
+    # Load/Create collection 
+    collection = documents_handler.load_collection(cfg.vector_db_dir)
+
+    current_id = 0
+    yellow = "\033[0;33m"
+    green = "\033[0;32m"
+    white = "\033[0;39m"
+
+    print(f"{yellow}---------------------------------------------------------------------------------")
+    print('Welcome to the DocBot. You are now ready to start interacting with your documents')
+    print('---------------------------------------------------------------------------------')
+
+    while True:
+        chat_history = []
+        chat_metadata = []
+        history_ids = []
+
+        messages = [{"role": "system", "content": cfg.openAI.chat_persona}]
+
+        input_text = input(f"{green}You: ")
+
+        if input_text == "exit" or input_text == "quit":
+            print('Exiting')
+            sys.exit()
+        if input_text == '':
+            continue
+
+
+        results = collection.query(
+            query_texts=[input_text],
+            where={"role": "assistant"},
+            n_results=2
+        )
+
+        # append the query result into the messages
+        for res in results['documents'][0]:
+            messages.append({"role": "user", "content": f"previous chat: {res}"})
+
+        # Add the user's input to the messages
+        messages.append({"role": "user", "content": input_text})
+        response = generate_response(vector_db, qa_chain, messages)
+
+        # Update chat history & metadata
+        chat_metadata.extend([{"role":"user"}, {"role": "assistant"}])
+        chat_history.extend([input_text, response])
+
+        # Update history IDs
+        current_id += 1
+        history_ids.extend([f"id_{current_id}", f"id_{current_id + 1}"])
+
+        # Add the conversation to the collection
+        collection.add(
+            documents=chat_history,
+            metadatas=chat_metadata,
+            ids=history_ids
+        )
+
+        # Print the bot's response
+        print(f"{white}Bot: {response}")
 
 if __name__ == "__main__":
     main()
--- llm_openai.py
+++ llm_openai.py
@@ -1,34 +1,66 @@
+import pprint
+from halo import Halo
 from omegaconf import DictConfig
 from langchain.chat_models import ChatOpenAI
+from langchain.vectorstores.chroma import Chroma
 from langchain.chains.question_answering import load_qa_chain
+from langchain.chains.combine_documents.base import BaseCombineDocumentsChain
 
-class OpenAIHandler:
 
-    def __init__ (self, cfg: DictConfig, temperature, max_tokens):
-        self.config = cfg.openAI
-        self.temperature = temperature
-        self.max_tokens = max_tokens
+class LLMOpenAI:
+    def __init__(self):
+        pass
 
-    def setup_chain(self, chain_type="stuff", verbose="false"):
+    def get_llm(self, cfg: DictConfig):
         '''
-        Sets up the Q & A chain.
+        Method to get the LLM model.
+
+        Args: 
+            cfg (DictConfig): The configuration object.
+
+        Returns:
+            The LLM model.
+        '''
+        try:
+            llm = ChatOpenAI(openai_api_key=cfg.openAI.api_key, model_name=cfg.openAI.model, 
+                   temperature=cfg.openAI.temperature, max_tokens=cfg.openAI.max_tokens)
+        except (AttributeError, KeyError) as e:
+            raise ValueError(f"Invalid or missing configuration: {e}")
+        
+        return llm
+    
+    def get_qa_chain(self, cfg: DictConfig):
+        '''
+        Method to get the Q & A chain.
 
         Args:
-            chain_type (str): The type of the chain.
-            verbose (str): Whether to print the logs or not.
+            cfg (DictConfig): The configuration object.
 
         Returns:
             The Q & A chain.
         '''
-        try:
-            
-            # Setup OpenAI chat
-            llm = ChatOpenAI(openai_api_key=self.config.api_key, model_name=self.config.model, 
-                                temperature=self.temperature, max_tokens=self.max_tokens) 
-            
-            # Setup Q & A Chain
-            loaded_qa_chain = load_qa_chain(llm=llm, chain_type=chain_type, verbose=verbose)
-            return loaded_qa_chain
+        llm = self.get_llm(cfg)
+        return load_qa_chain(llm=llm)
     
-        except (AttributeError, KeyError) as e:
-            raise ValueError(f"Invalid or missing configuration: {e}")
+    def generate_response(self, vector_db: Chroma, qa_chain: BaseCombineDocumentsChain, messages):
+        # Create a loading spinner
+        spinner = Halo(text='Loading...', spinner='dots')
+        spinner.start()
+
+        # Fetch latest user Input
+        latest_input = next((message for message in reversed(messages) if message.get("role") == "user"), None)
+
+        # Get matching documents based on input text
+        matching_docs = vector_db.similarity_search(latest_input["content"])
+        answer = qa_chain.run(input_documents=matching_docs, question=messages)
+
+        # Stop the spinner once the response is received
+        spinner.stop()
+
+        # Pretty-print the user message sent to the AI
+        pp = pprint.PrettyPrinter(indent=4)
+        print("Request:")
+        pp.pprint(messages)
+
+        return answer
+
