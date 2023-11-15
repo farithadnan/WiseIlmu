3e35e77f675b1eda033cba0983dc11f1a56e3662
Farith Adnan <22324133+farithadnan@users.noreply.github.com>
2023-11-15 14:33:00 +0800

    Add logging and error handling to chatbot launch

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
+import sys
 import hydra
+import logging
 from omegaconf import DictConfig
 from chatbot import ChatBot
 
-
 @hydra.main(version_base=None, config_path="config", config_name="config")
 def main(cfg: DictConfig):
-    # Launch the chatbot
-    chatbot = ChatBot(cfg)
-    chatbot.launch()
+
+    # Set up logging
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+    try:
+        # Launch the chatbot
+        chatbot = ChatBot(cfg)
+        chatbot.launch()
+    except Exception as e:
+        logging.error(f"{e}")
+        sys.exit()
 
 if __name__ == "__main__":
     main()
